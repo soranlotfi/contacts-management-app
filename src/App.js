1e2f91d0197d9931b4aa63f7fa2fe1b29a512f99
Soran Lotfi <soranit2000@gmail.com>
2023-03-14 23:10:46 +0330

    getting project ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+//bootstrap
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min"
+const App = ()=> {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>اپلیکیشن مدیریت مخاطبین</h3>
+        <button className="btn btn-secondary btn-outline-primary">افزودن</button>
     </div>
   );
 }
