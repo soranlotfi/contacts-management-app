e5685e526460c90d74ac7784026db797aba38a46
soran <soranit2000@gmail.com>
2023-09-18 08:08:19 +0330

    add some features and components to the project **

--- src/components/Contacts/Contact.jsx
+++ src/components/Contacts/Contact.jsx
@@ -1,47 +1,60 @@
-import {CURRENTLINE, CYAN, ORANGE, PURPLE, RED} from "../../assistants/colors";
+import { Link } from "react-router-dom";
 
-const Contact = () => {
-    return (<div className="col-md-6">
-        <div style={{backgroundColor: CURRENTLINE}} className="card my-2">
-            <div className="card-body">
-                <div className="row d-flex justify-content-around align-items-center">
-                    <div className="col-md-4 col-sm-4">
-                        <img src="https://via.placeholder.com/150" alt="" style={{border: `1px solid ${PURPLE}`}}
-                             className="img-fluid rounded"/>
-                    </div>
-                    <div className="col-md-7 col-sm-7">
-                        <ul className="list-group">
-                            <li className="list-group-item list-group-item-dark">نام و نام خانوادگی : {" "}
-                                <span className="fw-bold">
-                                                    سوران لطفی
-                                                </span>
-                            </li>
-                            <li className="list-group-item list-group-item-dark">شماره موبایل : {" "}
-                                <span className="fw-bold">
-                                                    09184451244
-                                                </span>
-                            </li>
-                            <li className="list-group-item list-group-item-dark">آدرس ایمیل: {" "}
-                                <span className="fw-bold">
-                                                    soranit2000@gmail.com
-                                                </span>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="col-md-1 col-sm-1 d-flex flex-column align-items-center">
-                        <button className="btn my-1" style={{backgroundColor: ORANGE}}>
-                            <span className="fa fa-eye"></span>
-                        </button>
-                        <button className="btn my-1" style={{backgroundColor: CYAN}}>
-                            <span className="fa fa-pencil"></span>
-                        </button>
-                        <button className="btn my-1" style={{backgroundColor: RED}}>
-                            <span className="fa fa-trash"></span>
-                        </button>
-                    </div>
-                </div>
+import { CURRENTLINE, CYAN, ORANGE, PURPLE, RED } from "../../helpers/colors";
+
+const Contact = ({ contact }) => {
+  return (
+    <div className="col-md-6">
+      <div style={{ backgroundColor: CURRENTLINE }} className="card my-2">
+        <div className="card-body">
+          <div className="row align-items-center d-flex justify-content-around">
+            <div className="col-md-4 col-sm-4">
+              <img
+                src={contact.photo}
+                alt={contact.fullname}
+                style={{ border: `1px solid ${PURPLE}` }}
+                className="img-fluid rounded"
+              />
             </div>
+            <div className="col-md-7 col-sm-7">
+              <ul className="list-group">
+                <li className="list-group-item list-group-item-dark">
+                  نام و نام خانوداگی :{"  "}
+                  <span className="fw-bold">{contact.fullname}</span>
+                </li>
+
+                <li className="list-group-item list-group-item-dark">
+                  شماره موبایل :{"  "}
+                  <span className="fw-bold">{contact.mobile}</span>
+                </li>
+
+                <li className="list-group-item list-group-item-dark">
+                  آدرس ایمیل :{"  "}
+                  <span className="fw-bold">{contact.email}</span>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-1 col-sm-1 d-flex flex-column align-items-center">
+              <Link
+                to={`/contacts/${contact.id}`}
+                className="btn my-1"
+                style={{ backgroundColor: ORANGE }}
+              >
+                <i className="fa fa-eye" />
+              </Link>
+
+              <button className="btn my-1" style={{ backgroundColor: CYAN }}>
+                <i className="fa fa-pen" />
+              </button>
+              <button className="btn my-1" style={{ backgroundColor: RED }}>
+                <i className="fa fa-trash" />
+              </button>
+            </div>
+          </div>
         </div>
-    </div>)
-}
+      </div>
+    </div>
+  );
+};
+
 export default Contact;
--- src/components/Contacts/SearchContact.jsx
+++ src/components/Contacts/SearchContact.jsx
@@ -1,18 +1,25 @@
-import {PURPLE} from "../../assistants/colors";
+import { PURPLE } from "../../helpers/colors";
 
-const searchContact = ()=>{
-    return(
-        <div className="col">
-            <div className="input-group mx-2 w-75" dir="ltr">
-                            <span className="input-group-text" id="basic-addon1"
-                                  style={{backgroundColor: PURPLE}}>
-                            <i className="fa fa-search"/>
-                            </span>
-                <input type="text" dir="rtl"
-                       className="form-control" placeholder="نام مخاطب ..." aria-label="search"
-                       aria-describedby="basic-addon1"/>
-            </div>
-        </div>
-    )
-}
-export default searchContact
+const SearchContact = () => {
+  return (
+    <div className="input-group mx-2 w-75" dir="ltr">
+      <span
+        className="input-group-text"
+        id="basic-addon1"
+        style={{ backgroundColor: PURPLE }}
+      >
+        <i className="fas fa-search" />
+      </span>
+      <input
+        dir="rtl"
+        type="text"
+        className="form-control"
+        placeholder="جستجوی مخاطب"
+        aria-label="Search"
+        aria-describedby="basic-addon1"
+      />
+    </div>
+  );
+};
+
+export default SearchContact;
