bc38c78dcec72ada1a9ef5b05253ff87f6eb8ed2
soran <soranit2000@gmail.com>
2023-09-18 08:07:26 +0330

    add some features and components to the project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,126 @@
-import {useState} from "react";
-import './App.css';
-//bootstrap
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.min"
-// components
-import {Navbar,Contacts} from "./components/";
+import { useState, useEffect } from "react";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
 
-const App = ()=> {
-    const [loading , setLoading] = useState(false)
-    const [getcontacts , setContacts] = useState([])
+import {
+  AddContact,
+  ViewContact,
+  Contacts,
+  EditContact,
+  Navbar,
+} from "./components";
+
+import {
+  getAllContacts,
+  getAllGroups,
+  createContact,
+} from "./services/contactService";
+
+import "./App.css";
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [forceRender, setForceRender] = useState(false);
+  const [getContacts, setContacts] = useState([]);
+  const [getGroups, setGroups] = useState([]);
+  const [getContact, setContact] = useState({
+    fullname: "",
+    photo: "",
+    mobile: "",
+    email: "",
+    job: "",
+    group: "",
+  });
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+
+        const { data: contactsData } = await getAllContacts();
+        const { data: groupsData } = await getAllGroups();
+
+        setContacts(contactsData);
+        setGroups(groupsData);
+
+        setLoading(false);
+      } catch (err) {
+        console.log(err.message);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+
+        const { data: contactsData } = await getAllContacts();
+
+        setContacts(contactsData);
+
+        setLoading(false);
+      } catch (err) {
+        console.log(err.message);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [forceRender]);
+
+  const createContactForm = async (event) => {
+    event.preventDefault();
+    try {
+      const { status } = await createContact(getContact);
+
+      if (status === 201) {
+        setContact({});
+        setForceRender(!forceRender);
+        navigate("/contacts");
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  const setContactInfo = (event) => {
+    setContact({
+      ...getContact,
+      [event.target.name]: event.target.value,
+    });
+  };
 
   return (
     <div className="App">
-        <Navbar/>
-        <Contacts contacts = {getcontacts} loading={loading}/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Navigate to="/contacts" />} />
+        <Route
+          path="/contacts"
+          element={<Contacts contacts={getContacts} loading={loading} />}
+        />
+        <Route
+          path="/contacts/add"
+          element={
+            <AddContact
+              loading={loading}
+              setContactInfo={setContactInfo}
+              contact={getContact}
+              groups={getGroups}
+              createContactForm={createContactForm}
+            />
+          }
+        />
+        <Route path="/contacts/:contactId" element={<ViewContact />} />
+        <Route path="/contacts/edit/:contactId" element={<EditContact />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,13 @@
-export {default as Contacts} from "./contact/Contacts"
-export {default as Contact} from "./contact/Contact"
-export {default as SearchContact} from "./contact/SearchContact"
-export {default as Navbar} from "./Navbar"
-export {default as Spinner} from "./Spinner"
+//Better way of importing
+
+/* Contact Components*/
+export { default as AddContact } from "./Contacts/AddContact";
+export { default as EditContact } from "./Contacts/EditContact";
+export { default as ViewContact } from "./Contacts/ViewContact";
+export { default as Contacts } from "./Contacts/Contacts";
+export { default as Contact } from "./Contacts/Contact";
+export { default as SearchContact } from "./Contacts/SearchContact";
+
+/* -- Other Components -- */
+export { default as Navbar } from "./Navbar";
+export { default as Spinner } from "./Spinner";
